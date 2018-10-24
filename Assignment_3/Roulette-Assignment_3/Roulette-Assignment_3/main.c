42016469edb18235d230368af7348b339c4099c7
Joyce Chin <chinjoyce30@gmail.com>
2018-10-25 07:00:29 +0800

    readdata,createlist,invertlist

--- Assignment_3/Roulette-Assignment_3/Roulette-Assignment_3/main.c
+++ Assignment_3/Roulette-Assignment_3/Roulette-Assignment_3/main.c
@@ -11,7 +11,62 @@
 #include <string.h>
 #include <assert.h>
 #define MAX_LEN 200
+typedef unsigned long int ui;
 
+
+typedef struct listnode *listptr;
+typedef struct listnode{
+    char data[MAX_LEN];
+    int mark;
+    listptr rlink;
+    listptr llink;
+}List;
+
+listptr invertList(listptr current,listptr trail){
+    listptr head=current;
+    do{
+        current->llink=trail;
+        trail=current;
+        current=current->rlink;
+    }while(current!=head);
+    do{
+        printf("%s\n",current->data);
+        current=current->llink;
+    }while(current!=head);
+    return 0;
+}
+
+listptr createList(listptr ptr, char *data){
+    listptr node=malloc(sizeof(List));
+    ui len=strlen(data)-1;
+    strncpy(node->data,data,len-1);
+    if(!ptr){
+        ptr=node;
+        ptr->rlink=ptr;
+    }
+    else{
+        node->rlink=ptr->rlink;
+        ptr->rlink=node;
+        ptr=node;
+    }
+    return ptr;
+}
+
+void readData(){
+    FILE *fp;
+    fp=fopen("data.csv","r");
+    assert(fp!=NULL);
+    char line[MAX_LEN];
+    listptr ptr=NULL,head=NULL;
+    while(fgets(line,MAX_LEN,fp)!=NULL){
+        if(!ptr){
+            ptr=createList(ptr,line);
+            head=ptr;
+        }
+        else ptr=createList(ptr, line);
+    }
+    head=invertList(head,ptr);
+}
 
 int menu(){
     printf("What do you want to do?\n");
@@ -27,6 +82,9 @@ int main()
 {
     while(1){
         int choice=menu();
-        
+        if(choice==1){
+            readData();
+            break;
+        }
     }
 }
