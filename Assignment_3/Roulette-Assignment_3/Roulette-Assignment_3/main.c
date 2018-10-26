4fcf581e458c6767d5a52d9d32ea33c7fdc0405e
Joyce Chin <chinjoyce30@gmail.com>
2018-10-26 12:11:33 +0800

    finish file process start input data

--- Assignment_3/Roulette-Assignment_3/Roulette-Assignment_3/main.c
+++ Assignment_3/Roulette-Assignment_3/Roulette-Assignment_3/main.c
@@ -22,6 +22,54 @@ typedef struct listnode{
     listptr llink;
 }List;
 
+void russianRoulette(int dir,listptr ptr,int total){
+    printf("Input skip number: ");
+    int skip;
+    scanf("%d",&skip);
+    int counter;
+    if(dir==1){
+        do{
+            printf("list length is: %d\n",total);
+            for(counter=0;counter<=skip;counter++){
+                do{
+                    ptr=ptr->rlink;
+                }while(ptr->mark);
+            }
+            printf("the data is popped: %s\n",ptr->data);
+            ptr->mark=1;
+        }while(total-- && total!=1);
+        while(ptr->mark) ptr=ptr->rlink;
+        printf("the last data is: %s\n",ptr->data);
+    }
+    else if(dir==2){
+        do{
+            printf("list length is: %d\n",total);
+            for(counter=0;counter<=skip;counter++){
+                do{
+                    ptr=ptr->llink;
+                }while(ptr->mark);
+            }
+            printf("the data is popped: %s\n",ptr->data);
+            ptr->mark=1;
+        }while(total-- && total!=1);
+        while(ptr->mark) ptr=ptr->llink;
+        printf("the last data is: %s\n",ptr->data);
+    }
+}
+
+int directionMenu(){
+    printf("choose your direction: \n");
+    printf("(1) right\n");
+    printf("(2) left\n");
+    int dir;
+    scanf("%d",&dir);
+    if(dir!=1 && dir!=2){
+        printf("There is no this choice\n");
+        directionMenu();
+    }
+    return dir;
+}
+
 listptr invertList(listptr current,listptr trail){
     listptr head=current;
     do{
@@ -29,17 +77,13 @@ listptr invertList(listptr current,listptr trail){
         trail=current;
         current=current->rlink;
     }while(current!=head);
-    do{
-        printf("%s\n",current->data);
-        current=current->llink;
-    }while(current!=head);
-    return 0;
+    return head;
 }
 
-listptr createList(listptr ptr, char *data){
+listptr createList(listptr ptr, char *data,ui len){
     listptr node=malloc(sizeof(List));
-    ui len=strlen(data)-1;
     strncpy(node->data,data,len-1);
+    node->mark=0;
     if(!ptr){
         ptr=node;
         ptr->rlink=ptr;
@@ -49,28 +93,50 @@ listptr createList(listptr ptr, char *data){
         ptr->rlink=node;
         ptr=node;
     }
+    printf("%s is appended\n",ptr->data);
     return ptr;
 }
 
-void readData(){
+listptr inputData(){
+    int num;
+    listptr ptr=NULL,head=NULL;
+    printf("How many nodes do you want to append: ");
+    scanf("%d\n",&num);
+    char name[MAX_LEN];
+    while(num--){
+        fgets(name,MAX_LEN,stdin);
+        ui len=strlen(name);
+        if(!ptr){
+            ptr=createList(ptr, name,len);
+            head=ptr;
+        }
+        else ptr=createList(ptr, name,len);
+    }
+    head=invertList(head, ptr);
+    return head;
+}
+
+listptr readData(){
     FILE *fp;
     fp=fopen("data.csv","r");
     assert(fp!=NULL);
     char line[MAX_LEN];
     listptr ptr=NULL,head=NULL;
     while(fgets(line,MAX_LEN,fp)!=NULL){
+        ui len=strlen(line)-1;
         if(!ptr){
-            ptr=createList(ptr,line);
+            ptr=createList(ptr,line,len);
             head=ptr;
         }
-        else ptr=createList(ptr, line);
+        else ptr=createList(ptr, line,len);
     }
     head=invertList(head,ptr);
+    return head;
 }
 
 int menu(){
     printf("What do you want to do?\n");
-    printf("(1) Read data drom a file\n");
+    printf("(1) Read data from a file\n");
     printf("(2) Append new nodes\n");
     printf("(0) Exit\n");
     int choice;
@@ -82,9 +148,30 @@ int main()
 {
     while(1){
         int choice=menu();
+        listptr ptr=NULL;
         if(choice==1){
-            readData();
-            break;
+            ptr=readData();
         }
+        else if(choice==2){
+            ptr=inputData();
+        }
+        listptr start=ptr;
+        if(ptr==NULL){
+            printf("The list is empty\n");
+            continue;
+        }
+        printf("the list datas are:\n");
+        printf("[");
+        int totalnode=0;
+        do{
+            if(start->rlink==ptr) printf("%s]",start->data);
+            else printf("%s, ",start->data);
+            start=start->rlink;
+            totalnode++;
+        }while(start!=ptr);
+        printf("\n");
+        int dir=directionMenu();
+        russianRoulette(dir,ptr,totalnode);
+        break;
     }
 }
