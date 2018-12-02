4b0a46d80b4accc22455b5ce759aaea1c5ac6c64
Joyce Chin <chinjoyce30@gmail.com>
2018-12-02 23:41:03 +0800

    start menu

--- Assignment_5/Graph-Assignment_5/Graph-Assignment_5/main.c
+++ Assignment_5/Graph-Assignment_5/Graph-Assignment_5/main.c
@@ -8,8 +8,19 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+int mainMenu(){
+    int choice;
+    printf("(1) Kruskal Algorithm\n");
+    printf("(2) Prim Algorithm\n");
+    printf("(3) Dijkstra Algorithm\n");
+    printf("Enter your choice: ");
+    scanf("%d",&choice);
+    return choice;
+}
+
+int main()
+{
+    int choice=mainMenu();
+    printf("%d\n",choice);
 }
