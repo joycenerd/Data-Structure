bef68fa703d782df61a892ced89c5809ba6b8682
Joyce Chin <chinjoyce30@gmail.com>
2018-12-08 14:08:48 +0800

    finish Dijkstra and add comments

--- Assignment_5/Graph-Assignment_5/Graph-Assignment_5/main.c
+++ Assignment_5/Graph-Assignment_5/Graph-Assignment_5/main.c
@@ -10,11 +10,12 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
-#include <limits.h>
 #define N 20
 #define MAX_WEIGHT 1000000
 typedef unsigned long int ui;
 
+
+// structure for undirected graph
 typedef struct{
     int vertex1;
     int vertex2;
@@ -22,7 +23,8 @@ typedef struct{
     int mark;
 }Graph;
 
-int choice(int dist[],int visit[],int totalnodes){
+// choosing smallest weight node
+int choose(int dist[],int visit[],int totalnodes){
     int mini=MAX_WEIGHT;
     int index=-1;
     int i;
@@ -35,10 +37,13 @@ int choice(int dist[],int visit[],int totalnodes){
     return index;
 }
 
+// find shortest path of directed graph with nonnegative weights
 void Dijkstra(FILE *fin){
     int totalnodes,i,j;
     fscanf(fin,"%d",&totalnodes);
     int cost[N][N];
+    // graph realtion matrix
+    // if two nodes doesn't have directed edge connected set the cost[][] as a large number
     for(i=0;i<totalnodes;i++){
         for(j=0;j<totalnodes;j++){
             fscanf(fin,"%d",&cost[i][j]);
@@ -48,32 +53,56 @@ void Dijkstra(FILE *fin){
     printf("Enter two vertices <start end>: ");
     int start,end;
     scanf("%d%d",&start,&end);
+    // check if the starting node is in the graph
+    if(start<1 || start>totalnodes) printf("Invalid starting node\n");
+    if(end<1 || end>totalnodes) printf("Invalid ending node\n");
     int visit[N]={0},dist[N];
-    for(i=0;i<totalnodes;i++) dist[i]=cost[start][i];
-    visit[start]=1;
+    // initialize all the distance to cost[start][]
+    for(i=0;i<totalnodes;i++) dist[i]=cost[start-1][i];
+    // mark starting node as visited
+    visit[start-1]=1;
+    // initialize starting node distance to 0
+    dist[start-1]=0;
+    // from reachable node choose the smallest weight total as the next node and visit it
     for(i=0;i<totalnodes-2;i++){
         int next=choose(dist,visit,totalnodes);
+        visit[next]=1;
+        int v;
+        // update all the other unvisited nodes' distance
+        for(v=0;v<totalnodes;v++){
+            if(!visit[v] && dist[next]+cost[next][v]<dist[v]){ dist[v]=dist[next]+cost[next][v];
+            }
+        }
     }
+    int mincost=dist[end-1];
+    printf("The cost from %d to %d is: ",start,end);
+    // if doesn't find a path from starting node to ending node, print -1
+    if(mincost==MAX_WEIGHT) printf("-1\n");
+    else printf("%d\n",mincost);
 }
 
+// compare function of qsort
 int cmp(const void *a, const void *b){
     int l = ((Graph *)a)->weight;
     int r = ((Graph *)b)->weight;
     return (l - r);
 }
 
+// find smallest edge from nodes in-set to node out-set
 int findEdge(int subset[],int totalnodes,Graph graph[],int count){
     int i;
     for(i=0;i<count;i++){
-        if(graph[i].mark==1) continue;
+        if(graph[i].mark==1) continue; // if edge already chosen skip
         int v=graph[i].vertex1;
         int w=graph[i].vertex2;
+        // choosing edge be one node in-set and another node out-set
         if(subset[v] && !subset[w]) return i;
         else if(!subset[v] && subset[w]) return i;
     }
     return 0;
 }
 
+// add edges (in-set node, out-set node)
 void Prim(FILE *fin){
     printf("The output is\n");
     int totalnodes;
@@ -82,6 +111,7 @@ void Prim(FILE *fin){
     int i,j,v,w;
     int edge[N][N];
     int count=0;
+    // construct graph relation struct
     for(i=0;i<totalnodes;i++){
         for(j=0;j<totalnodes;j++){
             fscanf(fin,"%d",&edge[i][j]);
@@ -93,27 +123,33 @@ void Prim(FILE *fin){
             }
         }
     }
+    // sort wieght from the smallest to the largest
     qsort(graph,count,sizeof(Graph),cmp);
     int subset[N]={0};
     int start;
+    // starting node --> the set only contains starting node in the beginning
     fscanf(fin,"%d",&start);
     subset[start-1]=1;
     int edgecount=1,mincost=0;
     while(edgecount<totalnodes){
+        // find smallest edge from nodes in-set to node out-set
         int index=findEdge(subset,totalnodes,graph,count);
         v=graph[index].vertex1;
         w=graph[index].vertex2;
+        // if chosen mark as 1
         graph[index].mark=1;
         printf("Edge %d: (%d,%d) cost: %d\n",edgecount++,v+1,w+1,edge[v][w]);
         mincost+=edge[v][w];
         subset[v]=subset[w]=1;
     }
-    printf("Minimum cost = %d\n\n",mincost);
+    printf("Minimum cost = %d\n",mincost);
 }
 
-
+// root is always smaller than 0
+// other nodes' value is the root index represent as in the same set
 int *maintainSubset(int v,int w,int arr[],int totalnodes){
     int i,parent;
+    // create subset
     if(arr[v]<0 && arr[w]<0){
         arr[v]+=arr[w];
         for(i=0;i<totalnodes;i++){
@@ -121,6 +157,7 @@ int *maintainSubset(int v,int w,int arr[],int totalnodes){
         }
         arr[w]=v;
     }
+    // merging two subsets
     else if(arr[v]<0 && arr[w]>=0){
         parent=arr[w];
         arr[v]+=arr[parent];
@@ -148,6 +185,7 @@ int *maintainSubset(int v,int w,int arr[],int totalnodes){
     return arr;
 }
 
+// check if the two nodes are in the same set or not
 int checkInSub(int v,int w, int arr[],int totalnodes){
     if(arr[v]<0 && arr[w]<0) return 1;
     else if(v==arr[w]) return -1;
@@ -155,6 +193,8 @@ int checkInSub(int v,int w, int arr[],int totalnodes){
     else return 1;
 }
 
+// add edges from smallest weight to the largest
+// check there is no circle while adding edges
 void Kruskal(FILE *fin){
     printf("The cost of Minimum Cost Spanning Tree are\n");
     Graph graph[N];
@@ -163,6 +203,7 @@ void Kruskal(FILE *fin){
     int i,j,v,w;
     int edge[N][N];
     int count=0;
+    // if the nodes are connected add to graph struct
     for(i=0;i<totalnodes;i++){
         for(j=0;j<totalnodes;j++){
             fscanf(fin,"%d",&edge[i][j]);
@@ -174,21 +215,28 @@ void Kruskal(FILE *fin){
             }
         }
     }
+    // sort the weight of the edges from smallest to largest
     qsort(graph,count,sizeof(Graph),cmp);
+    // subset is used to check if there will be circle while adding edges
     int subset[N];
     for(i=0;i<totalnodes;i++) subset[i]=-1;
     int edgecount=1,cur=0,mincost=0;
     while(edgecount<totalnodes){
         v=graph[cur].vertex1;
         w=graph[cur++].vertex2;
+        // if v and w are in the same set discard this edge
+        // because there will be cycle if adding this edge
         int check=checkInSub(v, w, subset, totalnodes);
+        // if not in the same subset then add this edge
+        // make v and w and all nodes related to them to the same set
         if(check==1){
             printf("Edge %d: (%d,%d) cost: %d\n",edgecount++,v+1,w+1,edge[v][w]);
             mincost+=edge[v][w];
+            // revise subset array everytime adding new edges
             *subset=*maintainSubset(v,w,subset,totalnodes);
         }
     }
-    printf("Minimum cost = %d\n\n",mincost);
+    printf("Minimum cost = %d\n",mincost);
 }
 
 int mainMenu(){
@@ -205,8 +253,11 @@ int mainMenu(){
 int main()
 {
     while(1){
+        // choose algorithm for finding the shortest path
         int choice=mainMenu();
+        if(choice==4) break;
         printf("please enter your file: ");
+        // read file
         char str[N],filename[N];
         scanf("\n");
         fgets(str,N,stdin);
@@ -218,5 +269,7 @@ int main()
         if(choice==1) Kruskal(fin);
         else if(choice==2) Prim(fin);
         else if(choice==3) Dijkstra(fin);
+        printf("\n");
     }
+    return 0;
 }
