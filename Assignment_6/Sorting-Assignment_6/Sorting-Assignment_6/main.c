0a941b9c0485efe1255ff29fa814a47117ca44c5
Joyce Chin <chinjoyce30@gmail.com>
2018-12-14 22:06:39 +0800

    complete comments

--- Assignment_6/Sorting-Assignment_6/Sorting-Assignment_6/main.c
+++ Assignment_6/Sorting-Assignment_6/Sorting-Assignment_6/main.c
@@ -14,6 +14,8 @@
 #define MAX_ARRAY 1000
 typedef unsigned long int ui;
 
+// swap function for quicksort
+// index will be the same after swap
 int swap2(int arr[],int a,int b){
     int tmp=arr[a];
     arr[a]=arr[b];
@@ -21,25 +23,38 @@ int swap2(int arr[],int a,int b){
     return *arr;
 }
 
+// using quick sort to sort the data
+// partition data recursively to smaller than pivot and larger than pivot
+// until the list is sorted
 void quickSort(int arr[],int l,int r,int n){
     int pivot,i,j,k;
     if(l<r){
-        i=l;j=r+1;pivot=l;
+        i=l;j=r+1;pivot=l; // pivot is the first element
         do{
+            // from the beginnig of the list, search for the first element>pivot
             do i++; while(arr[i]<arr[pivot]);
+            // from the tail of the list, search for the first element<pivot
             do j--; while(arr[j]>arr[pivot]);
+            // if i<j i and j swap
             if(i<j) *arr=swap2(arr,i,j);
         }while(i<j);
+        // j is the right postion for pivot
         *arr=swap2(arr,pivot,j);
         for(k=0;k<n;k++){
             if(k==n-1) printf("%3d\n",arr[k]);
             else printf("%3d ",arr[k]);
         }
-        quickSort(arr, l, j-1, n);
-        quickSort(arr, j+1, r, n);
+        // pivot left are all smaller than pivot
+        // pivot right are all larger than pivot
+        // partition to two list
+        // from left and right pick the new pivot and sort the rest of list
+        quickSort(arr, l, j-1, n);  // left list
+        quickSort(arr, j+1, r, n);  // right list
     }
 }
 
+// binary search for the insertion sort
+// find the first element > n in the sorted list
 int binarySearch(int data[], int l, int r, int searchnum) {
     while (r > l + 1) {
         int mid = (l + r) / 2;
@@ -50,22 +65,32 @@ int binarySearch(int data[], int l, int r, int searchnum) {
     else return r;
 }
 
+// using insertion sort to sort the data
+// n-1 sorted data, insert n to the right position in n-1 sorted data
 void insertionSort(int arr[],int n){
     printf("insertion sort:\n");
     int i,j,k;
+    // assume the first arr[0] is sorted so do from arr[1]
     for (i = 1; i < n; i++) {
         for(k=0;k<n;k++){
             if(k==n-1) printf("%3d\n",arr[k]);
             else printf("%3d ",arr[k]);
         }
         int cur = arr[i];
+        // if n>n-1 then it is sorted, so continue
         if (cur > arr[i - 1]) continue;
+        // if n<first element
+        // then shift all the element in sorted list one position to the right
+        // insert n to the first element
         else if (cur < arr[0]) {
             for (j = i - 1; j >= 0; j--) arr[j + 1] = arr[j];
             arr[0] = cur;
         }
+        // using binary search to search the sorted list for the right position to insert n
         else {
             int index = binarySearch(arr, 0, i - 1, cur);
+            // after search in order to insert
+            // data movement of data in sorted list is needed
             for (j = i - 1; j >= index; j--) arr[j + 1] = arr[j];
             arr[index] = cur;
         }
@@ -77,12 +102,16 @@ void insertionSort(int arr[],int n){
     printf("Total step: %d\n\n",n);
 }
 
+// c library swap
 void swap(int *a,int *b){
     int tmp=*a;
     *a=*b;
     *b=tmp;
 }
 
+// using bubble sort to sort the data
+// check with neighbor: j>j+1 then swap
+// the largest will be the first in the correct position
 void bubbleSort(int arr[],int n){
     printf("bubble sort:\n");
     int i,j,k;
@@ -95,9 +124,12 @@ void bubbleSort(int arr[],int n){
             else printf("%3d ",arr[k]);
         }
     }
+    // total step equal to data size-1
     printf("Total step: %d\n\n",n-1);
 }
 
+// using selection sort to sort the data
+// find the smallest from the unsorted list and swap to the right place
 void selectionSort(int arr[], int n){
     printf("selection sort:\n");
     int i,j,k;
@@ -112,6 +144,7 @@ void selectionSort(int arr[], int n){
             else printf("%3d ",arr[k]);
         }
     }
+    // total step equal to the data size
     printf("Total step: %d\n\n",n);
 }
 
@@ -165,6 +198,7 @@ int main()
             insertionSort(insertarr,arrlength);
             printf("quick sort:\n");
             quickSort(quickarr, 0, arrlength-1,arrlength);
+            printf("\n");
         }
     }
     return 0;
